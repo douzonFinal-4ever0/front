8fb6afa422f1066b8541b21f0a025dfddc77833b
esther <wjswls109@naver.com>
2023-10-09 04:16:22 +0900

    10/9 jest Router 파일 생성 (사용자 회의실 경로만 구현)

---
+++ src/routes/routes.js
@@ -0,0 +1,47 @@
+import { Suspense, lazy } from 'react';
+import { Navigate, useRoutes } from 'react-router-dom';
+import Layout from '../layouts/Layout';
+import Page404 from '../pages/Page404';
+
+const Loadable = (Component) => (props) => {
+  return (
+    <Suspense>
+      <Component {...props} />
+    </Suspense>
+  );
+};
+
+const Router = ({ isAdminMode, setIsAdminMode }) => {
+  return useRoutes([
+    {
+      path: '/mr',
+      element: (
+        <Layout isAdminMode={isAdminMode} setIsAdminMode={setIsAdminMode} />
+      ),
+      children: [
+        { element: <Navigate to="/mr/dashboard" />, index: true },
+        { path: 'dashboard', element: <MrUserDashboardPage /> },
+        { path: 'reservation', element: <MrUserRegistherPage /> }
+      ]
+    },
+    {
+      element: (
+        <Layout isAdminMode={isAdminMode} setIsAdminMode={setIsAdminMode} />
+      ),
+      children: [
+        { element: <Navigate to="/" />, index: true },
+        { path: '*', element: <Page404 /> }
+      ]
+    }
+  ]);
+};
+
+export default Router;
+
+const MrUserDashboardPage = Loadable(
+  lazy(() => import('../pages/mr_user/Dashboard'))
+);
+
+const MrUserRegistherPage = Loadable(
+  lazy(() => import('../pages/mr_user/Register'))
+);
