d5c80fd0a2b472e09e0d9f6b8bdb408c36dcd3f5
esther <wjswls109@naver.com>
2023-10-16 09:04:13 +0900

    10/16 jest 스타일 속성 추가

--- src/components/mr_user/Input.jsx
+++ src/components/mr_user/Input.jsx
@@ -10,10 +10,14 @@ const StyledInput = styled('input')(({ theme, sx }) => ({
   padding: '16px 14px',
   display: 'flex',
   width: '100%',
+  height: '56px',
   border: 'none',
   outline: 'none',
   borderRadius: '8px',
   backgroundColor: '#f5f5f5',
   fontSize: '14px',
+  '&:hover': {
+    outline: `1px solid ${theme.palette.grey['900']}`
+  },
   ...sx
 }));
