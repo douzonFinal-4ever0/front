265826c5627cb94e378cc67c283c656530adebf6
esther <wjswls109@naver.com>
2023-10-20 08:50:31 +0900

    10/20 jest 예약 정보 폼 생성

---
+++ src/pages/mr_user/rez/form/RezForm.jsx
@@ -0,0 +1,199 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setRezData } from '../../../../redux/reducer/mrUserSlice';
+
+import styled from '@emotion/styled';
+import {
+  TextField,
+  Grid,
+  Select,
+  MenuItem,
+  FormControlLabel,
+  Switch,
+  Typography,
+  Stack
+} from '@mui/material';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
+import Label from '../../../../components/mr_user/Label';
+import { palette } from '../../../../theme/palette';
+
+const RezForm = () => {
+  const dispatch = useDispatch();
+  const rezData = useSelector(setRezData).payload.mrUser;
+  const [age, setAge] = useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  return (
+    <form>
+      <Grid container spacing={1}>
+        {/* 회의 목적 */}
+        <Grid item container>
+          <StyledLabelGrid item xs={3}>
+            <Label htmlFor={'mPurpose'} text={'회의 목적'} />
+          </StyledLabelGrid>
+          <Grid item xs={9}>
+            <StyledTextInput
+              id="mPurpose"
+              variant="outlined"
+              value={rezData.mPurpose}
+            />
+          </Grid>
+        </Grid>
+
+        {/* 회의 종류 */}
+        <Grid item container>
+          <StyledLabelGrid item xs={3}>
+            <Label htmlFor={'mrType'} text={'회의 종류'} />
+          </StyledLabelGrid>
+          <Grid item xs={9}>
+            <StyledSelect
+              value={rezData.mType}
+              onChange={handleChange}
+              displayEmpty
+              inputProps={{ 'aria-label': 'Without label' }}
+              sx={{ width: '100%' }}
+            >
+              <MenuItem value="">선택</MenuItem>
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </StyledSelect>
+          </Grid>
+        </Grid>
+
+        {/* 반복 예약 토글 */}
+        <Grid item container sx={{ justifyContent: 'flex-end' }}>
+          <StyledSwitchLabel
+            value="start"
+            control={
+              <Switch
+                sx={{
+                  '& .MuiSwitch-track': {
+                    backgroundColor: palette.grey['400']
+                  }
+                }}
+              />
+            }
+            label="정기 예약"
+            labelPlacement="start"
+          />
+        </Grid>
+
+        {/* 예약 일자 */}
+        <Grid item container>
+          <StyledLabelGrid item xs={3}>
+            <Label htmlFor={'rezDate'} text={'예약 일자'} />
+          </StyledLabelGrid>
+          <Grid item xs={9}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DemoContainer components={['DatePicker']}>
+                <StyledDatePicker />
+              </DemoContainer>
+            </LocalizationProvider>
+          </Grid>
+        </Grid>
+
+        {/* 예약 시간 */}
+        <Grid item container>
+          <StyledLabelGrid item xs={3}>
+            <Label htmlFor={'rezTime'} text={'예약 시간'} />
+          </StyledLabelGrid>
+          <Grid item xs={9}>
+            <Stack direction={'row'} sx={{ alignItems: 'center' }}>
+              <StyledSelect
+                value={rezData.rezStartTime}
+                onChange={handleChange}
+                displayEmpty
+                inputProps={{ 'aria-label': 'Without label' }}
+                sx={{ width: '100%' }}
+              >
+                <MenuItem value="">선택</MenuItem>
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </StyledSelect>
+              <Typography sx={{ margin: '0 6px', display: 'block' }}>
+                ~
+              </Typography>
+              <StyledSelect
+                value={rezData.rezEndTime}
+                onChange={handleChange}
+                displayEmpty
+                inputProps={{ 'aria-label': 'Without label' }}
+                sx={{ width: '100%' }}
+              >
+                <MenuItem value="">선택</MenuItem>
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </StyledSelect>
+            </Stack>
+          </Grid>
+        </Grid>
+
+        {/* 총 인원수 */}
+        <Grid item container>
+          <StyledLabelGrid item xs={3}>
+            <Label htmlFor={'totPtCtn'} text={'총 인원수'} />
+          </StyledLabelGrid>
+          <Grid item xs={9}>
+            <StyledTextInput
+              id="totPtCtn"
+              variant="outlined"
+              type="number"
+              value={rezData.totPtCtn}
+            />
+          </Grid>
+        </Grid>
+      </Grid>
+    </form>
+  );
+};
+
+export default RezForm;
+
+const StyledLabelGrid = styled(Grid)(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center'
+}));
+
+const StyledTextInput = styled(TextField)(({ theme }) => ({
+  width: '100%',
+  borderRadius: '8px',
+  backgroundColor: theme.palette.grey['100'],
+  '& .MuiOutlinedInput-input': {
+    padding: '12px 10px'
+  }
+}));
+
+const StyledSelect = styled(Select)(({ theme }) => ({
+  width: '100%',
+  borderRadius: '8px',
+  backgroundColor: theme.palette.grey['100'],
+  '& .MuiOutlinedInput-input': {
+    padding: '12px 10px'
+  }
+}));
+
+const StyledSwitchLabel = styled(FormControlLabel)(({ theme }) => ({
+  '& .MuiFormControlLabel-label': {
+    fontSize: '14px',
+    fontWeight: 'bold'
+  }
+}));
+
+const StyledDatePicker = styled(DatePicker)(({ theme }) => ({
+  width: '100%',
+  backgroundColor: theme.palette.grey['100'],
+  borderRadius: '8px',
+  '& .MuiInputBase-input': {
+    padding: '12px 10px'
+  }
+}));
--- src/pages/mr_user/rez/section/RezSection.jsx
+++ src/pages/mr_user/rez/section/RezSection.jsx
@@ -5,105 +5,137 @@ import {
   AccordionDetails,
   AccordionSummary,
   Box,
+  Stack,
   Typography
 } from '@mui/material';
 import KeyboardArrowDownRoundedIcon from '@mui/icons-material/KeyboardArrowDownRounded';
 import AccessTimeRoundedIcon from '@mui/icons-material/AccessTimeRounded';
 import PersonRoundedIcon from '@mui/icons-material/PersonRounded';
 import PersonOutlineRoundedIcon from '@mui/icons-material/PersonOutlineRounded';
-import LocalHospitalOutlinedIcon from '@mui/icons-material/LocalHospitalOutlined';
+import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
 
 import SectionTitle from '../../../../components/mr_user/SectionTitle';
+import { palette } from '../../../../theme/palette';
+import RezForm from '../form/RezForm';
+import RectangleBtn from '../../../../components/mr_user/RectangleBtn';
 
 const RezSection = () => {
-  const [expanded, setExpanded] = useState(false);
+  const [expanded, setExpanded] = useState('rez');
 
   const handleChange = (panel) => (event, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
   };
   return (
-    <Box component={'section'}>
-      {/* 예약 정보 */}
-      <Accordion expanded={expanded === 'rez'} onChange={handleChange('rez')}>
-        <AccordionSummary
-          expandIcon={<KeyboardArrowDownRoundedIcon />}
-          aria-controls="panel1bh-content"
-          id="panel1bh-header"
-        >
-          <SectionTitle title="예약 정보" sx={{ fontSize: '16px' }}>
-            <AccessTimeRoundedIcon />
-          </SectionTitle>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
-            Aliquam eget maximus est, id dignissim quam.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      {/* 내부 참석자 */}
-      <Accordion
-        expanded={expanded === 'interPt'}
-        onChange={handleChange('interPt')}
-      >
-        <AccordionSummary
-          expandIcon={<KeyboardArrowDownRoundedIcon />}
-          aria-controls="panel1bh-content"
-          id="panel1bh-header"
-        >
-          <SectionTitle title="내부 참석자" sx={{ fontSize: '16px' }}>
-            <PersonRoundedIcon />
-          </SectionTitle>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
-            Aliquam eget maximus est, id dignissim quam.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      {/* 외부 참석자 */}
-      <Accordion
-        expanded={expanded === 'outerPt'}
-        onChange={handleChange('outerPt')}
-      >
-        <AccordionSummary
-          expandIcon={<KeyboardArrowDownRoundedIcon />}
-          aria-controls="panel1bh-content"
-          id="panel1bh-header"
-        >
-          <SectionTitle title="외부 참석자" sx={{ fontSize: '16px' }}>
-            <PersonOutlineRoundedIcon />
-          </SectionTitle>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
-            Aliquam eget maximus est, id dignissim quam.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      {/* 추가 장비 */}
-      <Accordion
-        expanded={expanded === 'outerPt'}
-        onChange={handleChange('outerPt')}
-      >
-        <AccordionSummary
-          expandIcon={<KeyboardArrowDownRoundedIcon />}
-          aria-controls="panel1bh-content"
-          id="panel1bh-header"
-        >
-          <SectionTitle title="추가 장비" sx={{ fontSize: '16px' }}>
-            <LocalHospitalOutlinedIcon />
-          </SectionTitle>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
-            Aliquam eget maximus est, id dignissim quam.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
+    <Box component={'section'} sx={{ height: '100%' }}>
+      <Stack sx={{ justifyContent: 'space-between', height: '100%' }}>
+        <Box>
+          {/* 예약 정보 */}
+          <Accordion
+            expanded={expanded === 'rez'}
+            onChange={handleChange('rez')}
+            sx={{
+              border: `3px solid ${
+                expanded === 'rez' ? palette.primary.dark : 'none'
+              }`
+            }}
+          >
+            <AccordionSummary
+              expandIcon={<KeyboardArrowDownRoundedIcon />}
+              aria-controls="panel1bh-content"
+              id="panel1bh-header"
+            >
+              <SectionTitle title="예약 정보" sx={{ fontSize: '16px' }}>
+                <AccessTimeRoundedIcon />
+              </SectionTitle>
+            </AccordionSummary>
+            <AccordionDetails>
+              <RezForm />
+            </AccordionDetails>
+          </Accordion>
+
+          {/* 내부 참석자 */}
+          <Accordion
+            expanded={expanded === 'interPt'}
+            onChange={handleChange('interPt')}
+            sx={{
+              border: `3px solid ${
+                expanded === 'interPt' ? palette.primary.dark : 'none'
+              }`
+            }}
+          >
+            <AccordionSummary
+              expandIcon={<KeyboardArrowDownRoundedIcon />}
+              aria-controls="panel1bh-content"
+              id="panel1bh-header"
+            >
+              <SectionTitle title="내부 참석자" sx={{ fontSize: '16px' }}>
+                <PersonRoundedIcon />
+              </SectionTitle>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Nulla facilisi. Phasellus sollicitudin nulla et quam mattis
+                feugiat. Aliquam eget maximus est, id dignissim quam.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+
+          {/* 외부 참석자 */}
+          <Accordion
+            expanded={expanded === 'outerPt'}
+            onChange={handleChange('outerPt')}
+            sx={{
+              border: `3px solid ${
+                expanded === 'outerPt' ? palette.primary.dark : 'none'
+              }`
+            }}
+          >
+            <AccordionSummary
+              expandIcon={<KeyboardArrowDownRoundedIcon />}
+              aria-controls="panel1bh-content"
+              id="panel1bh-header"
+            >
+              <SectionTitle title="외부 참석자" sx={{ fontSize: '16px' }}>
+                <PersonOutlineRoundedIcon />
+              </SectionTitle>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Nulla facilisi. Phasellus sollicitudin nulla et quam mattis
+                feugiat. Aliquam eget maximus est, id dignissim quam.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+
+          {/* 추가 장비 */}
+          <Accordion
+            expanded={expanded === 'supplies'}
+            onChange={handleChange('supplies')}
+            sx={{
+              border: `3px solid ${
+                expanded === 'supplies' ? palette.primary.dark : 'none'
+              }`
+            }}
+          >
+            <AccordionSummary
+              expandIcon={<KeyboardArrowDownRoundedIcon />}
+              aria-controls="panel1bh-content"
+              id="panel1bh-header"
+            >
+              <SectionTitle title="추가 장비" sx={{ fontSize: '16px' }}>
+                <AddCircleOutlineOutlinedIcon />
+              </SectionTitle>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Nulla facilisi. Phasellus sollicitudin nulla et quam mattis
+                feugiat. Aliquam eget maximus est, id dignissim quam.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        </Box>
+        <RectangleBtn type={'submit'} text={'예약하기'} />
+      </Stack>
     </Box>
   );
 };
