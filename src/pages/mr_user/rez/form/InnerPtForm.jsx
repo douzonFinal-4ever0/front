fe9a3020bf8cc0b371e0292205a2484725f4a4e1
esther <wjswls109@naver.com>
2023-10-20 11:15:00 +0900

    10/20 jest 추가 버튼 추가

--- src/pages/mr_user/rez/form/InnerPtForm.jsx
+++ src/pages/mr_user/rez/form/InnerPtForm.jsx
@@ -1,5 +1,16 @@
+import RectangleBtn from '../../../../components/mr_user/RectangleBtn';
+import { palette } from '../../../../theme/palette';
+
 const InnerPtForm = () => {
-  return <>11</>;
+  return (
+    <>
+      <RectangleBtn
+        type={'button'}
+        text={'참석자 추가'}
+        sx={{ padding: '14px 12px', backgroundColor: palette.grey['500'] }}
+      />
+    </>
+  );
 };
 
 export default InnerPtForm;
---
+++ src/pages/mr_user/rez/form/SuppliesForm.jsx
@@ -0,0 +1,14 @@
+import RectangleBtn from '../../../../components/mr_user/RectangleBtn';
+import { palette } from '../../../../theme/palette';
+
+const SuppliesForm = () => {
+  return (
+    <RectangleBtn
+      type={'button'}
+      text={'장비 추가'}
+      sx={{ padding: '14px 12px', backgroundColor: palette.grey['500'] }}
+    />
+  );
+};
+
+export default SuppliesForm;
