dfcd20bf4b988cc1417a3ef2c3a2df8156a55ed8
esther <wjswls109@naver.com>
2023-10-21 20:27:00 +0900

    10/21 jest sx 속성 추가

--- src/components/mr_user/Label.jsx
+++ src/components/mr_user/Label.jsx
@@ -1,12 +1,17 @@
 import styled from '@emotion/styled';
 
-const Label = ({ htmlFor, text }) => {
-  return <StyledLabel htmlFor={htmlFor}>{text}</StyledLabel>;
+const Label = ({ htmlFor, text, sx }) => {
+  return (
+    <StyledLabel htmlFor={htmlFor} sx={sx}>
+      {text}
+    </StyledLabel>
+  );
 };
 
 export default Label;
 
-const StyledLabel = styled('label')(({ theme }) => ({
+const StyledLabel = styled('label')(({ theme, sx }) => ({
   fontSize: '16px',
-  fontWeight: theme.typography.fontWeightBold
+  fontWeight: theme.typography.fontWeightBold,
+  ...sx
 }));
