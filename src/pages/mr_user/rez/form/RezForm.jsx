3f7bf6c9dd458d88553d65fcfe375fd52f1ce19a
esther <wjswls109@naver.com>
2023-10-22 02:14:08 +0900

    10/22 jest rezData 적용 수정

--- src/pages/mr_user/rez/form/RezForm.jsx
+++ src/pages/mr_user/rez/form/RezForm.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setRezData } from '../../../../redux/reducer/mrUserSlice';
+import dayjs from 'dayjs';
 
 import styled from '@emotion/styled';
 import {
@@ -19,13 +20,110 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 import Label from '../../../../components/common/Label';
-import { palette } from '../../../../theme/palette';
-import dayjs from 'dayjs';
 
 const RezForm = () => {
   const dispatch = useDispatch();
   const rezData = useSelector(setRezData).payload.mrUser;
-  console.log(rezData);
+  const { mPurpose, mType, rezDate, rezStartTime, rezEndTime, totPtCtn } =
+    rezData;
+
+  const meetingTypes = [
+    {
+      index: 0,
+      value: '프로젝트 회의'
+    },
+    {
+      index: 1,
+      value: '컨퍼런스'
+    },
+    {
+      index: 3,
+      value: '화상 회의'
+    },
+    {
+      index: 4,
+      value: '정기 회의'
+    }
+  ];
+
+  const times = [
+    {
+      index: 0,
+      value: '9:00'
+    },
+    {
+      index: 1,
+      value: '9:30'
+    },
+    {
+      index: 2,
+      value: '10:00'
+    },
+    {
+      index: 3,
+      value: '10:30'
+    },
+    {
+      index: 4,
+      value: '11:00'
+    },
+    {
+      index: 5,
+      value: '11:30'
+    },
+    {
+      index: 6,
+      value: '12:00'
+    },
+    {
+      index: 7,
+      value: '12:30'
+    },
+    {
+      index: 8,
+      value: '13:00'
+    },
+    {
+      index: 9,
+      value: '13:30'
+    },
+    {
+      index: 10,
+      value: '14:00'
+    },
+    {
+      index: 11,
+      value: '14:30'
+    },
+    {
+      index: 12,
+      value: '15:00'
+    },
+    {
+      index: 13,
+      value: '15:30'
+    },
+    {
+      index: 14,
+      value: '16:00'
+    },
+    {
+      index: 15,
+      value: '16:30'
+    },
+    {
+      index: 16,
+      value: '17:00'
+    },
+    {
+      index: 17,
+      value: '17:30'
+    },
+    {
+      index: 18,
+      value: '18:00'
+    }
+  ];
 
   // 회의 종류 셀렉트박스 이벤트
   const handleSelectBox = (e) => {
@@ -71,8 +169,8 @@ const RezForm = () => {
           <TextField
             id="mPurpose"
             variant="outlined"
-            value={rezData.mPurpose}
-            placeholder="회의 목적"
+            value={mPurpose}
+            placeholder="회의 목적을 입력하세요"
           />
         </Grid>
       </Grid>
@@ -83,11 +181,13 @@ const RezForm = () => {
           <Label htmlFor={'mrType'} text={'회의 종류'} />
         </StyledLabelGrid>
         <Grid item xs={9}>
-          <Select value={rezData.mType} onChange={handleSelectBox} displayEmpty>
+          <Select value={mType} onChange={handleSelectBox} displayEmpty>
             <MenuItem value="">선택</MenuItem>
-            <MenuItem value={'미팅'}>미팅</MenuItem>
-            <MenuItem value={'화상회의'}>화상회의</MenuItem>
-            <MenuItem value={'컨퍼런스'}>컨퍼런스</MenuItem>
+            {meetingTypes.map((item) => (
+              <MenuItem key={item.index} value={item.value}>
+                {item.value}
+              </MenuItem>
+            ))}
           </Select>
         </Grid>
       </Grid>
@@ -110,7 +210,11 @@ const RezForm = () => {
         <Grid item xs={9}>
           <LocalizationProvider dateAdapter={AdapterDayjs}>
             <DemoContainer components={['DatePicker']}>
-              <DatePicker onChange={handleDatePick} />
+              <DatePicker
+                onChange={handleDatePick}
+                format="YYYY-MM-DD"
+                value={rezDate}
+              />
             </DemoContainer>
           </LocalizationProvider>
         </Grid>
@@ -129,9 +233,9 @@ const RezForm = () => {
               displayEmpty
             >
               <MenuItem value="">선택</MenuItem>
-              <MenuItem value={10}>Ten</MenuItem>
-              <MenuItem value={20}>Twenty</MenuItem>
-              <MenuItem value={30}>Thirty</MenuItem>
+              {times.map((item) => (
+                <MenuItem value={item.value}>{item.value}</MenuItem>
+              ))}
             </Select>
             <Typography sx={{ margin: '0 6px', display: 'block' }}>
               ~
@@ -142,9 +246,9 @@ const RezForm = () => {
               displayEmpty
             >
               <MenuItem value="">선택</MenuItem>
-              <MenuItem value={10}>Ten</MenuItem>
-              <MenuItem value={20}>Twenty</MenuItem>
-              <MenuItem value={30}>Thirty</MenuItem>
+              {times.map((item) => (
+                <MenuItem value={item.value}>{item.value}</MenuItem>
+              ))}
             </Select>
           </Stack>
         </Grid>
