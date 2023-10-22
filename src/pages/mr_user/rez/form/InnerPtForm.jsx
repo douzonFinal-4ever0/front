1d5cce9edb84030bcb73147b3a5dba84a444bf6b
esther <wjswls109@naver.com>
2023-10-22 12:22:11 +0900

    10/22 jest 참석자 모달창 생성

--- src/pages/mr_user/rez/form/InnerPtForm.jsx
+++ src/pages/mr_user/rez/form/InnerPtForm.jsx
@@ -1,13 +1,32 @@
+import { useState } from 'react';
 import RectangleBtn from '../../../../components/common/RectangleBtn';
 import { palette } from '../../../../theme/palette';
+import Modal from '../../../../components/common/Modal';
+import InnerPtModalContent from '../modal/InnerPtModalContent';
 
 const InnerPtForm = () => {
+  const [openModal, setOpenModal] = useState(false);
+  const handleInnerPtBtn = () => {
+    setOpenModal(!openModal);
+  };
+
+  const handleModal = () => {
+    setOpenModal(!openModal);
+  };
+
   return (
     <>
       <RectangleBtn
         type={'button'}
         text={'참석자 추가'}
         sx={{ padding: '14px 12px', backgroundColor: palette.grey['500'] }}
+        handlebtn={handleInnerPtBtn}
+      />
+      <Modal
+        open={openModal}
+        handleModal={handleModal}
+        modalTitle={'참석자 추가'}
+        content={<InnerPtModalContent />}
       />
     </>
   );
