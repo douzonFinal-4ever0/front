4145ad457308f219d0147f4fac075f8458ebee7a
esther <wjswls109@naver.com>
2023-10-22 22:09:39 +0900

    10/22 jest 예약 버튼 이벤트 수정

--- src/pages/mr_user/rez/section/RezSection.jsx
+++ src/pages/mr_user/rez/section/RezSection.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setRezData } from '../../../../redux/reducer/mrUserSlice';
+import { setUserData } from '../../../../redux/reducer/userSlice';
 import {
   Accordion,
   AccordionDetails,
@@ -28,8 +29,13 @@ import axios from 'axios';
 const RezSection = () => {
   const dispatch = useDispatch();
   const rezData = useSelector(setRezData).payload.mrUser;
+  const userData = useSelector(setUserData).payload.user;
+  // 회의실 예약 리덕스 데이터
   const { m_name, m_type, rez_date, rez_start_time, rez_end_time, tot_pt_ctn } =
     rezData;
+  // 사용자 리덕스 데이터
+  const { mem_code, name } = userData;
+
   // 열린 Accordion 표시
   const [expanded, setExpanded] = useState('rez');
   // 예약버튼 활성화 여부
@@ -58,9 +64,10 @@ const RezSection = () => {
   // 예약 버튼 이벤트
   const handleBtnSubmit = async (e) => {
     e.preventDefault();
-
-    const res = await axios.post('/mr/rez');
-    console.log(rezData);
+    const mr_code = 'R022'; // *임시로 회의실 코드 지정
+    const data = { ...rezData, mem_code, mr_code };
+    const res = await axios.post('/mr/rez', data);
+    console.log(res);
   };
 
   return (
