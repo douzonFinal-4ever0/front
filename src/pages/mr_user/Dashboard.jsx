f16251e120c09d6ffb962a2709893774ad36ffb4
esther <wjswls109@naver.com>
2023-10-24 09:09:00 +0900

    10/24 jest 대시보드 레이아웃 테스트

--- src/pages/mr_user/Dashboard.jsx
+++ src/pages/mr_user/Dashboard.jsx
@@ -1,115 +1,129 @@
 import { useState } from 'react';
-import Toggle from '../../components/common/Toggle';
-import Modal from '../../components/common/Modal';
-import { Box, Button, Typography } from '@mui/material';
-import Searchbar from '../../components/common/Searchbar';
+import { useDispatch, useSelector } from 'react-redux';
+import { setRezData } from '../../redux/reducer/mrUserSlice';
+import dayjs from 'dayjs';
 
-// 모달창 콘텐츠 예시
-const ModalContentExample = () => {
-  return (
-    <Box sx={{ maxWidth: 600 }}>
-      <Typography variant="body2" color="text.secondary">
-        Lizards are a widespread group of squamate reptiles, with over 6,000
-        species, ranging across all continents except Antarctica Lizards are a
-        widespread group of squamate reptiles, with over 6,000 species, ranging
-        across all continents except Antarctica Lizards are a widespread group
-        of squamate reptiles, with over 6,000 species, ranging across all
-        continents except Antarctica Lizards are a widespread group of squamate
-        reptiles, with over 6,000 species, ranging across all continents except
-        Antarctica Lizards are a widespread group of squamate reptiles, with
-        over 6,000 species, ranging across all continents except Antarctica
-      </Typography>
-    </Box>
-  );
-};
+import styled from '@emotion/styled';
+import { Box, Button, Grid, Stack, Typography, TextField } from '@mui/material';
 
-const ModalActionBtns = () => {
-  const handleBtn = () => {
-    console.log('clicked');
-  };
-
-  return (
-    <Box>
-      <Button color="primary" onClick={handleBtn}>
-        Save
-      </Button>
-      <Button color="secondary" onClick={handleBtn}>
-        Cancel
-      </Button>
-    </Box>
-  );
-};
+import { meetingTypes } from '../../config';
+import { PAGE_INNER_PADDING } from '../../config';
+import { setUserData } from '../../redux/reducer/userSlice';
+import { palette } from '../../theme/palette';
+import SubHeader from '../../components/common/SubHeader';
+import MainContainer from '../../components/mr_user/MainContainer';
+import WrapContainer from '../../components/mr_user/WrapContainer';
+import Label from '../../components/common/Label';
+import Selectbox from '../../components/common/Selectbox';
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
 const Dashboard = () => {
-  // 토글 ------------------------------------------------------
-  // 선택된 토글 버튼 값
-  const [selectBtn, setSeletBtn] = useState('calender');
-  // 토글 버튼 이벤트
-  const handleToggleBtn = (event) => {
-    setSeletBtn(event.currentTarget.value);
-  };
-  // 토글버튼 데이터
-  const data = [
-    {
-      index: 0,
-      value: 'calender',
-      name: '캘린더'
-    },
-    {
-      index: 1,
-      value: 'map',
-      name: '지도'
-    },
-    {
-      index: 2,
-      value: 'table',
-      name: '테이블'
-    }
-  ];
+  const dispatch = useDispatch();
+  const userData = useSelector(setUserData).payload.user;
+  const rezData = useSelector(setRezData).payload.mrUser;
+  // 예약 리덕스 데이터
+  const { m_name, m_type, rez_date, rez_start_time, rez_end_time, tot_pt_ctn } =
+    rezData;
+  // 사용자 리덕스 데이터
+  const { name, position_name } = userData;
 
-  // 모달창------------------------------------------------------
-  // 모달창 열림 여부 값
-  const [open, setOpen] = useState(false);
-  // 모달창 열림닫힘 이벤트
-  const handleModal = () => setOpen(!open);
-
-  // 검색창------------------------------------------------------
-  const [value, setValue] = useState(null);
-  // 검색창 입력 이벤트
-  const handleInput = (e) => {
-    setValue(e.target.value);
+  // 회의 종류 셀렉트박스 이벤트
+  const handleSelectBox = (e) => {
+    const newRezData = { ...rezData, m_type: e.target.value };
+    dispatch(setRezData({ data: newRezData }));
   };
-  // 검색 클릭 이벤트
-  const handleSearchBtn = (e) => {
-    e.preventDefault();
-    console.log('asd');
+
+  // 예약 날짜 데이트피커 이벤트
+  const handleDatePick = (newValue) => {
+    const newRezData = {
+      ...rezData,
+      rez_date: newValue.format('YYYY-MM-DD')
+    };
+    dispatch(setRezData({ data: newRezData }));
   };
 
   return (
     <>
-      {/* 토글버튼 예시 */}
-      <Toggle
-        data={data}
-        selectBtn={selectBtn}
-        handleToggleBtn={handleToggleBtn}
-      />
-
-      {/* 모달창 예시 (*모달창 크기는 content 컴포넌트에서 지정!!!)*/}
-      <Button onClick={handleModal}>Open modal</Button>
-      <Modal
-        open={open}
-        handleModal={handleModal}
-        modalTitle={'모달 타이틀'}
-        content={<ModalContentExample />}
-        buttons={<ModalActionBtns />}
-      />
-
-      {/* 검색창 예시 (*검색창 크기는 겉에 감싸는 태그 width만큼 !!!)*/}
-      <Box sx={{ width: '300px' }}>
-        <Searchbar />
+      {/* <SubHeader title="대시보드" /> */}
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center'
+        }}
+      >
+        <StyledBigTitle>
+          {name} {position_name}님,
+          <br /> 오늘 회의 일정 3건 있습니다
+        </StyledBigTitle>
+        <MainContainer>
+          <Stack spacing={3}>
+            <WrapContainer bgcolor={'#fff'}>
+              <Grid
+                container
+                spacing={1}
+                sx={{ justifyContent: 'space-between' }}
+              >
+                <Grid item container xs={3} spacing={1}>
+                  <StyledLabelGrid item xs={4}>
+                    <Label htmlFor={'m_type'} text={'회의 종류'} />
+                  </StyledLabelGrid>
+                  <Grid item xs={8}>
+                    <Selectbox
+                      value={m_type}
+                      handleSelectBox={handleSelectBox}
+                      menuList={meetingTypes}
+                    />
+                  </Grid>
+                </Grid>
+                <Grid item xs={4}>
+                  <StyledLabelGrid item xs={3}>
+                    <Label htmlFor={'rez_date'} text={'예약 일자'} />
+                  </StyledLabelGrid>
+                  <Grid item xs={9} direction={'row'}>
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                      <DemoContainer components={['DatePicker']}>
+                        <DatePicker
+                          onChange={handleDatePick}
+                          format="YYYY-MM-DD"
+                          value={dayjs(rez_date)}
+                        />
+                      </DemoContainer>
+                    </LocalizationProvider>
+                  </Grid>
+                </Grid>
+                <Grid item xs={3}>
+                  1
+                </Grid>
+                <Grid item xs={2}>
+                  1
+                </Grid>
+              </Grid>
+            </WrapContainer>
+            <WrapContainer bgcolor={'#fff'}>
+              <Grid container spacing={3}></Grid>
+            </WrapContainer>
+          </Stack>
+        </MainContainer>
       </Box>
     </>
   );
 };
 
 export default Dashboard;
+
+const StyledBigTitle = styled(Typography)(({ theme }) => ({
+  paddingTop: PAGE_INNER_PADDING * 2,
+  paddingLeft: PAGE_INNER_PADDING,
+  paddingRight: PAGE_INNER_PADDING,
+  fontSize: '26px',
+  fontWeight: 'bold'
+}));
+
+const StyledLabelGrid = styled(Grid)(({ theme }) => ({
+  display: 'flex',
+  justifyContent: 'flex-end',
+  alignItems: 'center'
+}));
