0964a027ea7d11c4a001323960f523e1097ce02f
esther <wjswls109@naver.com>
2023-10-24 09:08:42 +0900

    10/24 jest 데이터 변경

--- src/pages/mr_user/rez/form/RezForm.jsx
+++ src/pages/mr_user/rez/form/RezForm.jsx
@@ -21,6 +21,7 @@ import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 import Label from '../../../../components/common/Label';
 import { palette } from '../../../../theme/palette';
+import { times, meetingTypes } from '../../../../config';
 import Selectbox from '../../../../components/common/Selectbox';
 
 const RezForm = () => {
@@ -28,104 +29,6 @@ const RezForm = () => {
   const rezData = useSelector(setRezData).payload.mrUser;
   const { m_name, m_type, rez_date, rez_start_time, rez_end_time, tot_pt_ctn } =
     rezData;
-
-  const meetingTypes = [
-    {
-      index: 0,
-      value: '프로젝트 회의'
-    },
-    {
-      index: 1,
-      value: '컨퍼런스'
-    },
-    {
-      index: 3,
-      value: '화상 회의'
-    },
-    {
-      index: 4,
-      value: '정기 회의'
-    }
-  ];
-
-  const times = [
-    {
-      index: 0,
-      value: '09:00'
-    },
-    {
-      index: 1,
-      value: '09:30'
-    },
-    {
-      index: 2,
-      value: '10:00'
-    },
-    {
-      index: 3,
-      value: '10:30'
-    },
-    {
-      index: 4,
-      value: '11:00'
-    },
-    {
-      index: 5,
-      value: '11:30'
-    },
-    {
-      index: 6,
-      value: '12:00'
-    },
-    {
-      index: 7,
-      value: '12:30'
-    },
-    {
-      index: 8,
-      value: '13:00'
-    },
-    {
-      index: 9,
-      value: '13:30'
-    },
-    {
-      index: 10,
-      value: '14:00'
-    },
-    {
-      index: 11,
-      value: '14:30'
-    },
-    {
-      index: 12,
-      value: '15:00'
-    },
-    {
-      index: 13,
-      value: '15:30'
-    },
-    {
-      index: 14,
-      value: '16:00'
-    },
-    {
-      index: 15,
-      value: '16:30'
-    },
-    {
-      index: 16,
-      value: '17:00'
-    },
-    {
-      index: 17,
-      value: '17:30'
-    },
-    {
-      index: 18,
-      value: '18:00'
-    }
-  ];
 
   // 회의 목적 이벤트
   const handleMName = (e) => {
