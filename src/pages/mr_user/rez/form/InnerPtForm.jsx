b3d839ed704a4e770e5d60b5fcbad67dc68fe100
esther <wjswls109@naver.com>
2023-10-28 21:49:30 +0900

    10/28 jest 참석자 추가 모달창 기능 개발

--- src/pages/mr_user/rez/form/InnerPtForm.jsx
+++ src/pages/mr_user/rez/form/InnerPtForm.jsx
@@ -1,40 +1,101 @@
-import { useState } from 'react';
-import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setRezData } from '../../../../redux/reducer/mrUserSlice';
+import axiosInstance from '../../../../utils/axios';
 
 import RectangleBtn from '../../../../components/common/RectangleBtn';
 import { palette } from '../../../../theme/palette';
-import Modal from '../../../../components/common/Modal';
-import InnerPtModalContent from '../modal/InnerPtModalContent';
-import { convertDataTree } from '../../../../utils/ConvertDataTree';
+import InnterPtModal from '../modal/InnerPtModal';
+
+import {
+  Avatar,
+  Box,
+  ListItem,
+  IconButton,
+  List,
+  ListItemAvatar,
+  ListItemText
+} from '@mui/material';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ClearIcon from '@mui/icons-material/Clear';
 
 const InnerPtForm = () => {
-  const [openModal, setOpenModal] = useState(false);
-  const [members, setMembers] = useState([]);
+  const dispatch = useDispatch();
+  const rezData = useSelector(setRezData).payload.mrUser;
+  const { mr_pt_list } = rezData;
 
+  // 모달창 오픈
+  const [openModal, setOpenModal] = useState(false);
+  // 서버에서 전달받은 멤버 리스트
+  const [members, setMembers] = useState([]);
+  // 모달창 적용 대상 리스트
+  const [selectMems, setSelectMems] = useState([]);
+
+  // 참석자 추가 버튼 이벤트
   const handleInnerPtBtn = async () => {
-    const res = await axios.get('/mr/mem');
-    const memList = convertDataTree(res.data); // 사원 리스트
-    setMembers(memList);
+    const res = await axiosInstance.get('/mr/mem');
+    setMembers(res.data);
+    setSelectMems(mr_pt_list);
     setOpenModal(!openModal);
   };
 
+  // 모달창 오픈 이벤트
   const handleModal = () => {
     setOpenModal(!openModal);
   };
 
+  // 멤버 삭제 버튼 이벤트
+  const handleDeleteMem = (e) => {
+    const lestMems = mr_pt_list.filter(
+      (user) => user.mem_code !== mr_pt_list[e.currentTarget.tabIndex].mem_code
+    );
+    const newRezData = { ...rezData, mr_pt_list: lestMems };
+    dispatch(setRezData({ data: newRezData }));
+  };
+
+  const MemList = ({ list }) => {
+    return (
+      <Box>
+        <List>
+          {list.map((user, index) => (
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar>
+                  <AccountCircleIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary={user.name} />
+              <IconButton
+                tabIndex={index}
+                edge="end"
+                aria-label="delete"
+                onClick={handleDeleteMem}
+              >
+                <ClearIcon />
+              </IconButton>
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    );
+  };
+
   return (
     <>
+      {mr_pt_list.length !== 0 ? <MemList list={mr_pt_list} /> : null}
       <RectangleBtn
         type={'button'}
         text={'참석자 추가'}
         sx={{ padding: '14px 12px', backgroundColor: palette.grey['500'] }}
         handlebtn={handleInnerPtBtn}
       />
-      <Modal
+      <InnterPtModal
         open={openModal}
         handleModal={handleModal}
-        modalTitle={'참석자 추가'}
-        content={<InnerPtModalContent list={members} />}
+        list={members}
+        ptList={mr_pt_list}
+        selectMems={selectMems}
+        setSelectMems={setSelectMems}
       />
     </>
   );
