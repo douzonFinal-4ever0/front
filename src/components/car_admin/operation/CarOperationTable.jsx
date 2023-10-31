f18c3a6ac0c6364f2048a0f18624586c286cc414
h2jin <tkdwls1229@naver.com>
2023-10-31 09:02:06 +0900

    10/31 lhj 운행 목록 조회 테이블

--- src/components/car_admin/operation/CarOperationTable.jsx
+++ src/components/car_admin/operation/CarOperationTable.jsx
@@ -1,11 +1,63 @@
 import { Box } from '@mui/system';
 import { DataGrid } from '@mui/x-data-grid';
+import { useEffect } from 'react';
+import axiosInstance from '../../../utils/axios';
+import { useState } from 'react';
+import {
+  Button,
+  Chip,
+  Divider,
+  IconButton,
+  Tooltip,
+  Typography
+} from '@mui/material';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
-const CarOperationTable = () => {
-  const operationData = [{ id: 1 }, { id: 2 }];
+const CarOperationTable = ({}) => {
+  const [operationData, setOperationData] = useState([]);
+
+  useEffect(() => {
+    axiosInstance
+      .get('/manager/car/operationList')
+      .then((res) => {
+        console.log(res.data);
+        const newData = res.data.map((item, index) => {
+          return {
+            operation_code: item.operation_code,
+            start_at: new Date(item.start_at).toLocaleDateString(),
+            car_info: {
+              car_code: item.car_code,
+              car_name: item.car_name,
+              type: item.type
+            },
+            mem_info: {
+              dept_name: item.dept_name,
+              position_name: item.position_name,
+              name: item.name
+            },
+            purpose: [
+              { reason: '일반업무', distance: item.nomal_biz_mileage },
+              { reason: '출 • 퇴근', distance: item.commute_mileage }
+            ],
+            loc: item.carLocList.map((data) => {
+              return {
+                loc_type: data.loc_type,
+                address: data.address
+              };
+            }),
+            memo: item.memo
+          };
+        });
+        setOperationData(newData);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   const columns = [
     {
-      field: 'maint_name',
+      field: 'start_at',
       headerName: '운행일시',
       width: 140,
       headerAlign: 'center',
@@ -13,49 +65,113 @@ const CarOperationTable = () => {
       align: 'center'
     },
     {
-      field: 'maint_start_at',
+      field: 'car_info',
       headerName: '차량',
-      width: 140,
+      width: 180,
       headerAlign: 'center',
       headerClassName: 'super-app-theme--header',
-      align: 'center'
+      align: 'center',
+      renderCell: (params) => (
+        <Box display="flex" width="100%" alignItems="center">
+          <Chip
+            label={params.value.type}
+            size="small"
+            sx={{ backgroundColor: '#90caf9', marginRight: '10px' }}
+          />
+          <Box>
+            <Typography variant="subtitle1">{params.value.car_code}</Typography>
+            <Typography
+              variant="body2"
+              sx={{
+                whiteSpace: 'nowrap',
+                overflow: 'hidden',
+                textOverflow: 'ellipsis',
+                maxWidth: '90px' // 원하는 최대 너비 설정
+              }}
+            >
+              {params.value.car_name}
+            </Typography>
+          </Box>
+        </Box>
+      )
     },
     {
-      field: 'maint_end_at',
+      field: 'mem_info',
       headerName: '운전자',
       type: '',
       width: 140,
       headerAlign: 'center',
       headerClassName: 'super-app-theme--header',
-      align: 'center'
+      align: 'center',
+      renderCell: (params) => (
+        <Box width="100%">
+          <Typography variant="subtitle1">{params.value.name}</Typography>
+          <Typography variant="body2">{`${params.value.dept_name} • ${params.value.position_name}`}</Typography>
+        </Box>
+      )
     },
     {
-      field: 'maint_cost',
-      headerName: '목적',
+      field: 'purpose',
+      headerName: '목적 • 거리',
       type: 'number',
       width: 140,
       align: 'center',
       headerAlign: 'center',
-      headerClassName: 'super-app-theme--header'
+      headerClassName: 'super-app-theme--header',
+      renderCell: (params) => (
+        <Box width="100%">
+          {params.value.map((item) => {
+            return (
+              <Box display="flex">
+                <Typography variant="body2" marginRight="5px">
+                  {item.reason}용
+                </Typography>
+                <Typography variant="subtitle2">{item.distance}km</Typography>
+              </Box>
+            );
+          })}
+        </Box>
+      )
     },
     {
-      field: 'pay_method',
-      headerName: '거리',
-      width: 140,
-      align: 'center',
-      headerAlign: 'center',
-      headerClassName: 'super-app-theme--header'
-    },
-    {
-      field: 'mc_name',
+      field: 'loc',
       headerName: '출•도착 정보',
-      width: 170,
+      width: 250,
       align: 'center',
       headerAlign: 'center',
-      headerClassName: 'super-app-theme--header'
+      headerClassName: 'super-app-theme--header',
+      renderCell: (params) => (
+        <Box width="100%">
+          {params.value.map((item) => {
+            return (
+              <Box sx={{ backgroundColor: '#f5f5f5' }} padding="0px 4px">
+                <Box display="flex" justifyContent="center" paddingTop="2px">
+                  <Typography variant="subtitle2" marginRight="15px">
+                    {item.loc_type}
+                  </Typography>
+                  <Tooltip title={item.address} placement="top-start">
+                    <Typography
+                      variant="body2"
+                      sx={{
+                        whiteSpace: 'nowrap',
+                        overflow: 'hidden',
+                        textOverflow: 'ellipsis',
+                        maxWidth: '140px' // 원하는 최대 너비 설정
+                      }}
+                    >
+                      {item.address}
+                    </Typography>
+                  </Tooltip>
+                </Box>
+                <Divider sx={{ backgroundColor: '#616161' }} />
+              </Box>
+            );
+          })}
+        </Box>
+      )
     },
     {
-      field: 'mem_info', // 추후 수정 필요
+      field: 'memo',
       headerName: '메모',
       sortable: false,
       width: 140,
@@ -64,14 +180,19 @@ const CarOperationTable = () => {
       align: 'center'
     },
     {
-      field: 'memo',
       headerName: '운행 경로',
       headerAlign: 'center',
       align: 'center',
       headerClassName: 'super-app-theme--header',
-      width: 140
+      width: 140,
+      renderCell: (params) => (
+        <IconButton color="primary" aria-label="add to shopping cart">
+          <LocationOnIcon />
+        </IconButton>
+      )
     }
   ];
+
   return (
     <Box
       sx={{
@@ -85,7 +206,7 @@ const CarOperationTable = () => {
       <DataGrid
         rows={operationData}
         columns={columns}
-        getRowId={(row) => row.id}
+        getRowId={(row) => row.operation_code}
         initialState={{
           pagination: {
             paginationModel: { page: 0, pageSize: 5 }
@@ -96,6 +217,7 @@ const CarOperationTable = () => {
         }}
         pageSizeOptions={[5, 10]}
         sx={{ borderRadius: '2px' }}
+        rowHeight={90}
       />
     </Box>
   );
