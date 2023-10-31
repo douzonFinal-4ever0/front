f84ad126817180281e7123c4beca798c152389bf
esther <wjswls109@naver.com>
2023-11-01 00:13:56 +0900

    11/1 jest 즐겨찾기 등록 기능 완료

--- src/pages/mr_user/bm/BmPage.jsx
+++ src/pages/mr_user/bm/BmPage.jsx
@@ -16,8 +16,7 @@ import BmMemModal from './modal/BmMemModal';
 const BmPage = () => {
   // 사용자 정보 (**** 추후 작업 예정)
   const userData = useSelector(setUserData).payload.user;
-  // *임시로 사번 지정
-  const mem_code = 'MEM023';
+  const { mem_code } = userData;
   // 삭제할 멤버 번호 리스트
   const [deleteMemCodeList, setDeleteMemCodeList] = useState([]);
   // 선택한 탭
@@ -35,7 +34,6 @@ const BmPage = () => {
   const [members, setMembers] = useState([]);
   // 모달창 적용 대상 리스트
   const [selectMems, setSelectMems] = useState([]);
-
   // 모달창 오픈
   const [openModal, setOpenModal] = useState(false);
 
@@ -44,41 +42,42 @@ const BmPage = () => {
     setOpenModal(!openModal);
   };
 
+  // 즐겨찾기 조회 API
+  const bmGroupMemApi = async () => {
+    const res = await axiosInstance.get(`/mr/mem/bm?mem_code=${mem_code}`);
+    const { data } = res;
+
+    // 개별 멤버
+    const memResult = data.filter((item) => item.bm_group_name === null);
+    // 그룹 & 그룹 멤버
+    const groupResult = data.filter((item) => item.bm_group_name !== null);
+
+    // 그룹핑
+    const groupedData = [];
+    groupResult.forEach((item) => {
+      const { bm_group_name, ...rest } = item;
+
+      // 해당 bm_group_code가 이미 존재하는지 확인
+      const existingGroup = groupedData.find(
+        (group) => group.bm_group_name === bm_group_name
+      );
+
+      if (existingGroup) {
+        // 이미 존재하는 경우 mem_list 배열에 추가
+        existingGroup.mem_list.push(rest);
+      } else {
+        // 새로운 그룹 생성
+        const newGroup = { bm_group_name, mem_list: [rest] };
+        groupedData.push(newGroup);
+      }
+    });
+
+    setMemList([...memResult]);
+    setGroupList([...groupedData]);
+  };
+
   // 첫 로드 시 서버로부터 즐겨찾기 데이터 가져오기
   useEffect(() => {
-    const bmGroupMemApi = async () => {
-      const res = await axiosInstance.get(`/mr/mem/bm?mem_code=${mem_code}`);
-      const { data } = res;
-
-      // 개별 멤버
-      const memResult = data.filter((item) => item.bm_group_name === null);
-      // 그룹 & 그룹 멤버
-      const groupResult = data.filter((item) => item.bm_group_name !== null);
-
-      // 그룹핑
-      const groupedData = [];
-      groupResult.forEach((item) => {
-        const { bm_group_name, ...rest } = item;
-
-        // 해당 bm_group_code가 이미 존재하는지 확인
-        const existingGroup = groupedData.find(
-          (group) => group.bm_group_name === bm_group_name
-        );
-
-        if (existingGroup) {
-          // 이미 존재하는 경우 mem_list 배열에 추가
-          existingGroup.mem_list.push(rest);
-        } else {
-          // 새로운 그룹 생성
-          const newGroup = { bm_group_name, mem_list: [rest] };
-          groupedData.push(newGroup);
-        }
-      });
-
-      setMemList([...memResult]);
-      setGroupList([...groupedData]);
-    };
-
     bmGroupMemApi();
   }, []);
 
@@ -183,6 +182,7 @@ const BmPage = () => {
         </MainContainer>
       </Box>
       <BmMemModal
+        selectTab={selectTab}
         open={openModal}
         handleModal={handleModal}
         list={members}
@@ -190,6 +190,7 @@ const BmPage = () => {
         selectMems={selectMems}
         setSelectMems={setSelectMems}
         master={mem_code}
+        bmGroupMemApi={bmGroupMemApi}
       />
     </>
   );
