fdc302fc03c7b259c5f9ea95b137f6631a9cad75
h2jin <tkdwls1229@naver.com>
2023-11-09 09:11:46 +0900

    11/09 lhj 아이콘 로고 수정

--- src/components/car_admin/operation/MileageLogModal.jsx
+++ src/components/car_admin/operation/MileageLogModal.jsx
@@ -449,8 +449,8 @@ const MileageLogModal = ({ style, handleModalClose }) => {
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="25px"
-                height="25px"
+                width="20px"
+                height="20px"
                 viewBox="0,0,256,256"
               >
                 <g
