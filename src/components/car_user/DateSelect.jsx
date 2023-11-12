e8c52ab95a9af9ac520b3f12b0ec50e987fb30a1
lkw <gewonlee99@gmail.com>
2023-11-13 08:31:55 +0900

    11/13 lkw

--- src/components/car_user/DateSelect.jsx
+++ src/components/car_user/DateSelect.jsx
@@ -1,10 +1,12 @@
 import {
   Box,
   Collapse,
+  Grid,
   List,
   ListItemButton,
   ListItemText,
   ListSubheader,
+  Stack,
   Typography
 } from '@mui/material';
 import { useState } from 'react';
@@ -87,29 +89,70 @@ const DateSelect = ({ setDateRange, dateFilter, setDateFilter }) => {
           <ListItemText>상세 입력</ListItemText>
         </ListItemButton>
         <Collapse in={opnColl2} sx={{ mt: 1 }}>
-          {' '}
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <Box>
-              <DatePicker
-                sx={{
-                  '& .MuiOutlinedInput-root': {
-                    height: '40px',
-                    width: '100%'
-                  },
-                  '& .MuiInputBase-inputAdornedEnd': { fontSize: '15px' }
-                }}
-                format="YYYY-MM-DD"
-                onChange={(e) => {
-                  console.log(e.format('YYYY-MM-DD'));
+          <Stack sx={{ rowGap: '10px' }}>
+            <Grid item container xs={12} spacing={2}>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <Grid item xs={5.5}>
+                  <Box>
+                    <DatePicker
+                      sx={{
+                        '& .MuiOutlinedInput-root': {
+                          height: '40px',
+                          width: '100%'
+                        },
+                        '& .MuiInputBase-inputAdornedEnd': { fontSize: '15px' }
+                      }}
+                      format="YYYY-MM-DD"
+                      onChange={(e) => {
+                        console.log(e.format('YYYY-MM-DD'));
 
-                  setStarDate(e.format('YYYY-MM-DD'));
-                  // setFilter({
-                  //   ...filter,
-                  //   startDate: e.format('YYYY-MM-DD')
-                  // });
-                }}
-              />
-            </Box>
+                        setStarDate(e.format('YYYY-MM-DD'));
+                        // setFilter({
+                        //   ...filter,
+                        //   startDate: e.format('YYYY-MM-DD')
+                        // });
+                      }}
+                    />
+                  </Box>
+                </Grid>
+                <Grid
+                  item
+                  xs={1}
+                  sx={{
+                    display: 'flex',
+                    alignItems: 'center'
+                    // margin: '0px 10px'
+                  }}
+                >
+                  <Box>
+                    <Typography variant="h6">-</Typography>
+                  </Box>
+                </Grid>
+                <Grid item xs={5.5}>
+                  <Box>
+                    <DatePicker
+                      sx={{
+                        '& .MuiOutlinedInput-root': {
+                          height: '40px',
+                          width: '100%'
+                        },
+                        '& .MuiInputBase-inputAdornedEnd': { fontSize: '15px' }
+                      }}
+                      format="YYYY-MM-DD"
+                      onChange={(e) => {
+                        console.log(e.format('YYYY-MM-DD'));
+
+                        setStarDate(e.format('YYYY-MM-DD'));
+                        // setFilter({
+                        //   ...filter,
+                        //   startDate: e.format('YYYY-MM-DD')
+                        // });
+                      }}
+                    />
+                  </Box>
+                </Grid>
+              </LocalizationProvider>
+            </Grid>
             <Box
               sx={{
                 display: 'flex',
@@ -119,35 +162,51 @@ const DateSelect = ({ setDateRange, dateFilter, setDateFilter }) => {
             >
               <Typography variant="h6">-</Typography>
             </Box>
-          </LocalizationProvider>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <Box>
-              <DatePicker
-                sx={{
-                  '& .MuiOutlinedInput-root': {
-                    height: '40px',
-                    width: '100%'
-                  },
-                  '& .MuiInputBase-inputAdornedEnd': { fontSize: '15px' }
-                }}
-                format="YYYY-MM-DD"
-                size="small"
-                onChange={(e) => {
-                  setEndDate(e.format('YYYY-MM-DD'));
-                  // setFilter({
-                  //   ...filter,
-                  //   endDate: e.format('YYYY-MM-DD')
-                  // });
-                }}
-              />
-            </Box>
-          </LocalizationProvider>
-          <RectangleBtn
-            text={'검색'}
-            category={'register'}
-            sx={{ padding: '14px 12px' }}
-            handlebtn={selectDate}
-          />
+
+            <Grid item container xs={12} spacing={3}>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <Grid item xs={1}></Grid>
+                <Grid item xs={9}>
+                  <Box>
+                    <DatePicker
+                      sx={{
+                        '& .MuiOutlinedInput-root': {
+                          height: '40px',
+                          width: '100%'
+                        },
+                        '& .MuiInputBase-inputAdornedEnd': { fontSize: '15px' }
+                      }}
+                      format="YYYY-MM-DD"
+                      size="small"
+                      onChange={(e) => {
+                        setEndDate(e.format('YYYY-MM-DD'));
+                        // setFilter({
+                        //   ...filter,
+                        //   endDate: e.format('YYYY-MM-DD')
+                        // });
+                      }}
+                    />
+                  </Box>
+                </Grid>
+                <Grid
+                  item
+                  xs={2}
+                  sx={{
+                    display: 'flex',
+                    alignItems: 'center'
+                  }}
+                >
+                  <Typography variant="body2">까지</Typography>
+                </Grid>
+              </LocalizationProvider>
+            </Grid>
+            <RectangleBtn
+              text={'검색'}
+              category={'register'}
+              sx={{ padding: '14px 12px' }}
+              handlebtn={selectDate}
+            />
+          </Stack>
         </Collapse>
       </List>
     </Box>
